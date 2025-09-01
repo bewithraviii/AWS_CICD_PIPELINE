2813e999efb73228f4670824f2cc495cfe66662d
sfxhchauhan <hchauhan@seamfix.com>
2025-09-01 16:58:10 +0530

    updated folder and files

--- AWS_CICD_PIPELINE/lambda/index.js
+++
@@ -1,7 +0,0 @@
-exports.lambda_handler = async (event) => {
-    console.log('Hello from Lambda!');
-    return {
-      statusCode: 200,
-      body: JSON.stringify('Hello from Lambda via Node.js!'),
-    };
-};
